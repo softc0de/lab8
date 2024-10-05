f6bd1a8c80c4667623c0315848440f70f789ddf4
Захар Грошев <fatalitydo@gmail.com>
2024-10-05 10:26:24 +0300

    Изменил приветствия в зависимости от текущего времени пользователя

--- hello.pas
+++ hello.pas
@@ -1,3 +1,12 @@
 ﻿begin
-  print('Привет, мир!');
+  var nowtime := readinteger('Введите текущий час (от 0 до 23): ');
+  assert((nowtime >= 0) and (nowtime < 24));
+  
+  case nowtime of
+    4..10: print('Доброе утро, мир!');
+    11..16: print('Добрый день, мир!');
+    17..22: print('Доброй вечер, мир!')
+    else 
+      print('Доброй ночи, мир!');
+  end;
 end.
